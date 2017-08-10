489f3207241c0e1a269e8e1683237bd37ba97529
Zulu55 <jzuluaga55@gmail.com>
2017-08-09 21:36:04 -0500

    Finsih

---
+++ ForeignExchangeMac2/ForeignExchangeMac2Page.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+
+namespace ForeignExchangeMac2
+{
+    public partial class ForeignExchangeMac2Page : ContentPage
+    {
+        public ForeignExchangeMac2Page()
+        {
+            InitializeComponent();
+
+            ConvertButton.Clicked += ConvertButton_Clicked;
+        }
+
+        void ConvertButton_Clicked(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty(PesosEntry.Text))
+            {
+				DisplayAlert("Error", "You must enter a value in  pesos..", "Accept");
+                return;
+			}
+
+            decimal pesos = 0;
+            if (!decimal.TryParse(PesosEntry.Text, out pesos))
+            {
+				DisplayAlert("Error", "You must enter a value numeric in  pesos..", "Accept");
+				return;
+			}
+
+			var dollars = pesos / 3003.003M;
+			var euros = pesos / 3531.05105M;
+			var pounds = pesos / 3907.23724M;
+
+			DollarsEntry.Text = string.Format("${0:N2}", dollars);
+            EurosEntry.Text = string.Format("€{0:N2}", euros);
+            PoundsEntry.Text = string.Format("£{0:N2}", pounds);
+		}
+    }
+}
